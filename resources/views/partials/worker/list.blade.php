670823bc1bd2286a54cb3f465cf02ba65ef91469
milospetkovic <mail2milos@yahoo.com>
2019-04-12 18:39:45 +0200

    adjusted responsiveness of worker tables

--- resources/views/partials/worker/list.blade.php
+++ resources/views/partials/worker/list.blade.php
@@ -20,54 +20,58 @@
                             <input class="show-inactive" type="checkbox" @if($showinactive) checked @endif name="showinactive" value="1" @click="$(this).closest('form').submit();"> Prikaži i neaktivne
                         </label>
 
-                        <table class="table" id="table">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Kompanija</th>
-                                    <th>Prezime</th>
-                                    <th>Ime</th>
-                                    <th>Početak ugovora</th>
-                                    <th>Kraj ugovora</th>
-                                    <th>JMBG</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
+                        <div class="table-responsive">
 
-                            <tbody>
+                            <table class="table table-bordered table-hover" id="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Kompanija</th>
+                                        <th>Prezime</th>
+                                        <th>Ime</th>
+                                        <th>Početak ugovora</th>
+                                        <th>Kraj ugovora</th>
+                                        <th>JMBG</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
 
-                            @foreach($workers as $ind => $worker)
+                                <tbody>
 
-                                <?php
-                                $row_class = '';
-                                if (strtotime($worker->contract_end) <= (time() + (2 * 86400))) {
-                                    $row_class = "bg-warning";
-                                }
-                                if ($worker->contract_end <= date('Y-m-d')) {
-                                    $row_class = "bg-danger";
-                                }
-                                ?>
+                                @foreach($workers as $ind => $worker)
 
-                                <tr class="{{ $row_class }}">
-                                    <td>{{ $ind + 1 }}.</td>
-                                    <td><a href="/company/show/{{ $worker->company_id }}">{{ $worker->company_name }}</a></td>
-                                    <td><a href="/worker/show/{{ $worker->id }}">{{ $worker->last_name }}</a></td>
-                                    <td><a href="/worker/show/{{ $worker->id }}">{{ $worker->first_name }}</a></td>
-                                    <td>{{ date('d.m.Y', strtotime($worker->contract_start)) }}</td>
-                                    <td>{{ date('d.m.Y', strtotime($worker->contract_end)) }}</td>
-                                    <td>{{ $worker->jmbg }}</td>
-                                    <td>
-                                        @if ($worker->inactive)
-                                            <span class="badge text-danger text-muted" style="background: red !important">NEAKTIVAN</span>
-                                        @else
-                                            <span class="badge text-success text-muted" style="background: green !important; opacity: 0.5;">Aktivan</span>
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
+                                    <?php
+                                    $row_class = '';
+                                    if (strtotime($worker->contract_end) <= (time() + (2 * 86400))) {
+                                        $row_class = "bg-warning";
+                                    }
+                                    if ($worker->contract_end <= date('Y-m-d')) {
+                                        $row_class = "bg-danger";
+                                    }
+                                    ?>
 
-                            </tbody>
-                        </table>
+                                    <tr class="{{ $row_class }}">
+                                        <td>{{ $ind + 1 }}.</td>
+                                        <td><a href="/company/show/{{ $worker->company_id }}">{{ $worker->company_name }}</a></td>
+                                        <td><a href="/worker/show/{{ $worker->id }}">{{ $worker->last_name }}</a></td>
+                                        <td><a href="/worker/show/{{ $worker->id }}">{{ $worker->first_name }}</a></td>
+                                        <td>{{ date('d.m.Y', strtotime($worker->contract_start)) }}</td>
+                                        <td>{{ date('d.m.Y', strtotime($worker->contract_end)) }}</td>
+                                        <td>{{ $worker->jmbg }}</td>
+                                        <td>
+                                            @if ($worker->inactive)
+                                                <span class="badge text-danger text-muted" style="background: red !important">NEAKTIVAN</span>
+                                            @else
+                                                <span class="badge text-success text-muted" style="background: green !important; opacity: 0.5;">Aktivan</span>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                                </tbody>
+                            </table>
+
+                        </div>
 
                     </form>
 
