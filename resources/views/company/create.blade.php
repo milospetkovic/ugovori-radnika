1a3506997b411f5579cee6ae8770e89bcb79970e
milospetkovic <elitasoft.web@gmail.com>
2019-08-18 15:54:57 +0200

    create company - added cancel button

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -18,6 +18,8 @@
 
                     <div class="mrg-t-10">
                         <button type="submit" class="btn btn-primary m-r"><span class="fa fa-check"></span> Sačuvaj</button>
+
+                        <a class="btn btn-default" href="{{ action('Worker\WorkerController@listWorkers') }}" class="btn btn-default">Prekini</a>
                     </div>
 
                 </form>
