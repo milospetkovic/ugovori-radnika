865cb65a32cdb2ef0657df934c3411bff64642fc
milospetkovic <elitasoft.web@gmail.com>
2019-08-18 17:45:21 +0200

    fixed layouts

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -2,34 +2,30 @@
 
 @section('content')
 
+    <div class="col-xs-12">
 
+        <div class="panel panel-default">
 
-        <div class="col-xs-12">
+            <div class="panel-heading">Komitent: {{ $company->name }}</div>
 
-            <div class="panel panel-default">
+            <div class="panel-body">
 
-                <div class="panel-heading">Komitent: {{ $company->name }}</div>
+                <a href="/company/show/{{ $company->id }}/worker/create" class="btn btn-sm btn-primary m-r">Dodaj radnika</a>
 
-                <div class="panel-body">
+                <a href="{{ action('HomeController@index') }}" class="btn btn-sm btn-default"><i class="glyphicon glyphicon-home"></i> Početna strana</a>
 
-                    <a href="/company/show/{{ $company->id }}/worker/create" class="btn btn-sm btn-primary m-r">Dodaj radnika</a>
-
-                    <a href="{{ action('HomeController@index') }}" class="btn btn-sm btn-default"><i class="glyphicon glyphicon-home"></i> Početna strana</a>
-
-                    <a onclick="if (!confirm('Da li zaista želite da obrišete komitenta?')) return false;"  href="{{ action('Company\CompanyController@delete', ['company_id' => $company->id ]) }}" class="btn btn-sm btn-danger pull-right"><span class="fa fa-check"></span>Obriši</a>
-
-                </div>
+                <a onclick="if (!confirm('Da li zaista želite da obrišete komitenta?')) return false;"  href="{{ action('Company\CompanyController@delete', ['company_id' => $company->id ]) }}" class="btn btn-sm btn-danger pull-right"><span class="fa fa-check"></span>Obriši</a>
 
             </div>
 
         </div>
 
-        <div class="clearfix">
+    </div>
 
-            @include('partials.worker.list')
+    <div class="clearfix">
 
-        </div>
+        @include('partials.worker.list')
 
-
+    </div>
 
 @endsection
