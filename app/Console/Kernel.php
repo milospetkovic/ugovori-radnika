5b6df33bd16b89c63f86839d386f4a386de9e67a
milospetkovic <elitasoft.web@gmail.com>
2020-11-12 00:12:14 +0100

    register new command in the kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\FirebaseSendnotification;
+use App\Console\Commands\WorkersUnactivate;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        FirebaseSendnotification::class
+        FirebaseSendnotification::class,
+        WorkersUnactivate::class
     ];
 
     /**
